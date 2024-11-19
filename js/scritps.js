a44601c7cd36765d4488944bab58e91a94b516bb
Erika Cruz <erikahcruz96@gmail.com>
2024-11-19 17:31:30 -0300

    Primeiro commit

---
+++ js/scritps.js
@@ -0,0 +1,47 @@
+//Selecionar elementos
+
+const valor = document.querySelector(".valor");
+const qualidade = document.querySelector("#quald");
+const calcBtn = document.querySelector(".calc-btn");
+const gorjeta = document.querySelector(".result-valor");
+const valorTotal = document.querySelector(".result-total");
+
+
+//Eventos
+
+calcBtn.addEventListener("click", calcularGorjeta)
+
+//Funções
+
+
+function calcularGorjeta(e){
+    e.preventDefault();
+    const valorValue = parseFloat(valor.value); 
+    const qualidadeValue = qualidade.value;
+
+    if(isNaN(valorValue) || valorValue <= 0){
+        alert("Por favor preencha os dados!");
+        return;
+    }
+
+    let resultTotal;
+    switch(qualidadeValue){
+        case "exe":
+            resultTotal = (10 * valorValue)/100;
+            break;
+        case "otimo":
+            resultTotal = (8 * valorValue)/100;
+            break;
+        case "bom":
+            resultTotal = (5 * valorValue)/100;
+            break;
+        case "ruim":
+            resultTotal = (2 * valorValue)/100;
+            break;
+    }
+
+    gorjeta.value = `R$ ${resultTotal.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`;
+    valorTotal.value = `R$ ${(resultTotal + valorValue).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`;
+}
+
+
